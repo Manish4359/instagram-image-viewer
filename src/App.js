d9337bd1ac5ff756b2e4b83223497c82a221447c
Manish4359 <manishkr.official4359@gmail.com>
2022-10-26 16:30:02 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Login from './components/login/login.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
      <Login/>
     </div>
   );
--- src/components/login/login.component.jsx
+++ src/components/login/login.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './login.styles.css'
 import { useRef } from 'react'
+import { useState } from 'react';
+
+import { loginData } from '../../loginData';
 
 function Login() {
 
@@ -11,8 +14,33 @@ function Login() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        const username=userameRef.current.value
+        const password=passwordRef.current.value
+
         console.log(userameRef.current.value)
         console.log(passwordRef.current.value)
+
+        if(username==='' && password===''){
+            e.target.getElementsByClassName('no-username')[0].style.visibility='visible'
+            e.target.getElementsByClassName('no-pass')[0].style.visibility='visible'
+
+            return
+        }
+
+        if(username!==loginData.username || password!==loginData.password){
+            e.target.getElementsByClassName('login-error')[0].style.visibility='visible'
+
+            e.target.getElementsByClassName('no-username')[0].style.visibility='hidden'
+            e.target.getElementsByClassName('no-pass')[0].style.visibility='hidden'
+        }
+        else{
+            alert('logged in successfully!!')
+            e.target.getElementsByClassName('login-error')[0].style.visibility='hidden'
+
+        }
+
+
+
     }
     return (
         <div className='login-container'>
@@ -20,8 +48,10 @@ function Login() {
             <h3 className='login-heading'>Login</h3>
             <form className='login-form' onSubmit={handleSubmit}>
                 <input type="text" id='username' name='username' placeholder='UserName *' ref={userameRef} />
+                <span className='error no-username' style={{visibility:'hidden'}}>required</span>
                 <input type="password" id='password' name='password' placeholder='Password *' ref={passwordRef} />
-
+                <span className='error no-pass' style={{visibility:'hidden'}}>required</span>
+                <span className='error login-error' style={{visibility:'hidden'}}>Incorrect username or password</span>
                 <button type="submit" className='login-btn'>Login</button>
             </form>
         </div>
